1d3f23352896f77447dd428d178777dd20e701ff
ValverdeMA <66846351+ValverdeMA@users.noreply.github.com>
2021-03-15 15:06:10 -0600

    Practica Interpolacion

---
+++ Practicas/MetodosInterpolaciones.py
@@ -0,0 +1,70 @@
+import numpy as np
+from sympy import symbols, init_printing, expand, lambdify
+import matplotlib.pyplot as plt
+x = symbols('x')
+init_printing()
+
+
+class NumMethods:
+
+    def __init__(self):
+        self.puntosX, self.puntosY = self.llenarPuntos()
+        self.matriz = []
+        self.expr = 0
+
+    def llenarPuntos(self):
+        numPuntos = int(input('Cuántos puntos vas a ingresar? '))
+        x = []
+        y = []
+        for i in range(numPuntos):
+            x.append(float(input('x = ')))
+            y.append(float(input('y = ')))
+        return x, y
+
+    def NewtonInterpol(self):
+        self.matriz.append(self.puntosX)
+        self.matriz.append(self.puntosY)
+        self.diferenciasFinitas(len(self.puntosX) - 1)
+        print(self.matriz)
+        temp = 1
+        expr = self.matriz[1][0]
+        for i in range(2, len(self.matriz)):
+            for j in range(0, i-1):
+                temp *= x-self.matriz[0][j]
+            expr += self.matriz[i][0]*temp
+            temp = 1
+
+        print('polinomio calculado:')
+        print(expand(expr))
+        self.expr += expr
+        self.Grafica()
+
+    def diferenciasFinitas(self, actual):
+        if actual == 0:
+            return self.puntosY
+        self.diferenciasFinitas(actual - 1)
+        col = []
+
+        for i in range(len(self.puntosY) - actual):
+            fx1 = self.matriz[-1][i+1]
+            fx0 = self.matriz[-1][i]
+            x1 = self.matriz[0][len(self.matriz) - 1 + i]
+            x0 = self.matriz[0][i]
+            col.append(self.diferencia(fx1, fx0, x1, x0))
+        if not col in self.matriz:
+            self.matriz.append(col)
+        return col
+
+    def diferencia(self, fx1, fx0, x1, x0):
+        return (fx1 - fx0)/(x1-x0)
+
+    def Grafica(self):
+        a = np.linspace(min(self.puntosX), max(self.puntosX), 1000)
+        b = [self.expr.subs(x, i) for i in a]
+        plt.plot(a, b)
+        plt.scatter(self.puntosX, self.puntosY)
+        plt.show()
+
+
+NM = NumMethods()
+NM.NewtonInterpol()
